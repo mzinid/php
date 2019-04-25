f4bc04e24657761dd867ad33578534659ef345b4
m awad <36506720+awadrisk@users.noreply.github.com>
2019-04-25 21:50:11 +0200

    php

---
+++ head.php
@@ -0,0 +1,65 @@
+<head>
+<script type="8eb11aafa6911205fbb0c98e-text/javascript">
+    var host = "startbootstrap.com";
+    if ((host == window.location.host) && (window.location.protocol != "https:"))
+      window.location.protocol = "https";
+  </script>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="author" content="Start Bootstrap">
+<meta name="google-site-verification" content="37Tru9bxB3NrqXCt6JT5Vx8wz2AJQ0G4TkC-j8WL3kw">
+
+<title>
+    Creative - Theme Preview - Start Bootstrap
+  </title>
+
+<meta name="description" content="">
+
+<link rel="canonical" href="https://startbootstrap.com/previews/creative/">
+
+<link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-6jHF7Z3XI3fF4XZixAuSu0gGKrXwoX/w3uFPxC56OtjChio7wtTGJWRW53Nhx6Ev" crossorigin="anonymous">
+
+<link rel="stylesheet" href="/assets/style-theme.css">
+
+<link type="application/atom+xml" rel="alternate" href="https://startbootstrap.com/feed.xml" title="startbootstrap" />
+<script type="8eb11aafa6911205fbb0c98e-text/javascript">
+  // Google Analytics Tracking Script
+  (function(i, s, o, g, r, a, m) {
+    i['GoogleAnalyticsObject'] = r;
+    i[r] = i[r] || function() {
+      (i[r].q = i[r].q || []).push(arguments)
+    }, i[r].l = 1 * new Date();
+    a = s.createElement(o),
+      m = s.getElementsByTagName(o)[0];
+    a.async = 1;
+    a.src = g;
+    m.parentNode.insertBefore(a, m)
+  })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+  ga('create', 'UA-38417733-17', 'startbootstrap.com');
+  ga('send', 'pageview');
+</script>
+<meta name='ir-site-verification-token' value='-28223945'>
+
+<meta property="og:title" content="Creative - Theme Preview">
+<meta property="og:site_name" content="Start Bootstrap">
+<meta property="og:type" content="website">
+<meta property="og:description" content="">
+<meta property="og:image" content="https://startbootstrap.com/assets/img/branding/og-start-bootstrap.png">
+<meta property="og:url" content="https://startbootstrap.com/previews/creative/">
+<meta property="og:image:alt" content="Creative - Theme Preview">
+
+<meta name="twitter:card" content="summary">
+<meta name="twitter:image" content="https://startbootstrap.com/assets/img/branding/start-bootstrap-logo-500x500.png">
+<meta name="twitter:site" content="@SBootstrap">
+
+<link rel="apple-touch-icon" sizes="180x180" href="/assets/img/icons/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/assets/img/icons/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/assets/img/icons/favicon-16x16.png">
+<link rel="manifest" href="/assets/img/icons/site.webmanifest">
+<link rel="mask-icon" href="/assets/img/icons/safari-pinned-tab.svg" color="#dd3d31">
+<meta name="msapplication-TileColor" content="#dd3d31">
+<meta name="theme-color" content="#ffffff">
+</head>
